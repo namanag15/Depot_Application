3bb61ec40c9ea44669689c0f69b12d74e8b0074c
NamanAg <cooldudenaman15@gmail.com>
2017-07-04 05:38:22 +0530

    Error handling and cart improved

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    @line_item = @cart.add_product(product.id)
+    
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -57,9 +57,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_item = LineItem.find(params[:id])
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to :back, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -72,6 +73,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
