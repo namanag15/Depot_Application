ac1232b035bc5b56cc833daf99508c9b8daf6b1d
NamanAg <cooldudenaman15@gmail.com>
2017-07-14 12:02:26 +0530

    Ajax working properly and jQuery error removed

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-        format.js
+        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -62,6 +62,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to :back, notice: 'Line item was successfully destroyed.' }
+      format.js   { @current_item = @line_item }
       format.json { head :no_content }
     end
   end
